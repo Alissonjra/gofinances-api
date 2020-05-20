209af5b2da83f6f7b6480dc06a6c8b21bb2058a9
Alisson Junior <53801159+Alissonjra@users.noreply.github.com>
2020-05-20 17:40:01 -0300

    Desafio concluido

---
+++ src/database/migrations/1589986686853-AddFKTransactionsCategories.ts
@@ -0,0 +1,37 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class AddFKTransactionsCategories1589986686853
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'transactions',
+      new TableColumn({
+        name: 'category_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        name: 'FKTransactionCategory',
+        columnNames: ['category_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'categories',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('transactions', 'FKTransactionCategory');
+    await queryRunner.dropColumn('transactions', 'category_id');
+  }
+}
